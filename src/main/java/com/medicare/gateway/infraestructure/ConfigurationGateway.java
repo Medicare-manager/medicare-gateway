1e366709d496c5ac14b6114289413b2b00c1ee6e
silvia <silviafernandes974@gmail.com>
2024-12-14 17:52:11 -0300

    feat: validar request no serviço de kotlin

--- src/main/java/com/medicare/gateway/infraestructure/ConfigurationGateway.java
+++ src/main/java/com/medicare/gateway/infraestructure/ConfigurationGateway.java
@@ -1,19 +1,25 @@
 package com.medicare.gateway.infraestructure;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class ConfigurationGateway {
+
+    private final AuthenticationFilter filter;
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("medicine", r -> r.path("/medicine/**")
+                        .filters(f -> f.filter(filter))
                         .uri("http://localhost:8082/"))
                 .route("user", r -> r.path("/auth/**")
+                        .filters(f -> f.filter(filter))
                         .uri("http://localhost:8081/"))
                 .build();
     }
---
+++ src/main/java/com/medicare/gateway/infraestructure/RouterValidator.java
@@ -0,0 +1,20 @@
+package com.medicare.gateway.infraestructure;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouterValidator {
+
+    public static final List<String> openApiEndpoints = List.of(
+            "/auth/register", "/auth/login2", "/auth/login", "/medicine/teste"
+    );
+
+    public Predicate<ServerHttpRequest> isSecured =
+            request -> openApiEndpoints
+                    .stream()
+                    .noneMatch(uri -> request.getURI().getPath().equals(uri));
+}
